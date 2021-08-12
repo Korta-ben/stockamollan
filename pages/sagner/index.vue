2cb933883642674bab5bb438b9e312181b55858d
Ashish Aryal <ashish@kortaben.se>
2021-08-12 11:43:54 +0200

    api references changed

--- pages/sagner/index.vue
+++ pages/sagner/index.vue
@@ -19,7 +19,7 @@ export default {
 
   async asyncData ({ params }) {
     const { data } = await axios.get(
-      `http://stockamollan.local/wp-json/wp/v2/sagner`
+      `https://api.stockamollan.guide/wp-json/wp/v2/sagner`
     )
     let stories = data
     return { stories }
