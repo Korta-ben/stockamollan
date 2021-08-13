bd26208763e338b1c0f493df05f5301229c9fe01
Ashish Aryal <ashish@kortaben.se>
2021-08-13 13:14:05 +0200

    trying my luck

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,5 @@
 module.exports = {
-  purge: {
-    content: [
-      `components/**/*.{vue,js}`,
-      `layouts/**/*.vue`,
-      `pages/**/*.vue`,
-      `plugins/**/*.{js,ts}`,
-      `nuxt.config.{js,ts}`
-    ]
-  },
+  purge: {},
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors:{
