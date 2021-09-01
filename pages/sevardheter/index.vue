7c114dd6599f0de151948bb816acbe2f0975d861
Ashish Aryal <ashish@kortaben.se>
2021-09-01 13:58:11 +0200

    landmark filter added

--- pages/sevardheter/index.vue
+++ pages/sevardheter/index.vue
@@ -1,8 +1,28 @@
 <template>
   <main class="pt-48 siteMaxWidth xl:px-36 lg:px-28 ">
     <h1 class=" lora font-medium text-3xl text-center px-12 pb-8">Sevärheterna i Stockamöllan</h1>
+    <div class="max-w-2xl mx-auto text-center px-5">
+      <p >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
+      </p>
+      <h3 class="text-stGreen text-base font-bold leading-6 pt-11">FILTRERA:</h3>
+      <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+        <button v-for="trail in trails" :key="trail.index"
+                class="border-2 border-stGreen text-stGreen font-semibold text-lg leading-7 py-4"
+                @click="filterLandmakrs(trail.id)"
+        >
+
+          {{ trail.acf.colour.label }} slinga
+        </button>
+      </div>
+
+    </div>
+
+
+
 
     <section class="p-7 lg:py-7">
+      <h1 v-if="!landmarks.length" class="text-center">Sorry, No landmarks here!</h1>
       <TheLandmarkCard v-for="landmark in landmarks" :key="landmark.id" :landmark="landmark"
                        class="mb-7 rounded-xl hover:shadow-2xl transition"/>
     </section>
@@ -16,8 +36,26 @@ export default {
   data() {
     return {
       landmarks:this.$store.getters.getLandmarks,
+      trails: this.$store.getters.getHikingTrails,
+
     }
   },
+
+  methods:{
+    filterLandmakrs(trailId){
+
+      this.landmarks = this.$store.getters.getLandmarks.filter( l =>
+         l.acf.vandringslederna.filter( v => v.ID == trailId).pop()
+    )
+
+
+
+
+
+
+
+        }
+  }
 
 
 
