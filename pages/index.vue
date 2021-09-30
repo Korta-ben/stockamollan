9241fe22e420552133e963815f77e5ab18a84827
Ashish Aryal <ashish@kortaben.se>
2021-09-30 13:55:37 +0200

    arrow on the slider installed

--- pages/index.vue
+++ pages/index.vue
@@ -90,7 +90,7 @@ export default {
           "slidesToShow": 4,
           "slidesToScroll": 1,
           "initialSlide": 0,
-          "arrows":false,
+          "arrows":true,
           "responsive": [
 
             {
