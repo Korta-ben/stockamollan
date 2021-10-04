38af50cac5384bd11e2c869fd84ee0f39b303f5d
Ashish Aryal <ashish@kortaben.se>
2021-10-04 11:58:31 +0200

    bug fixes

--- components/blocks/TheTipsBlock.vue
+++ components/blocks/TheTipsBlock.vue
@@ -7,10 +7,16 @@
     <div v-html="tipCard.body" class="text-center tracking-tight lg:text-left px-7 lg:pr-20 pt-5 text-stCream text-base pb-16">
 
     </div>
-    <div v-html="tipCard.button_text"
-         class="text-center py-3 leading-6 mx-8 text-stCream text-lg font-medium border-2 border-stCream">
+    <div
+         class="text-center  leading-6 mx-8 text-stCream text-lg font-medium border-2 border-stCream flex" >
+      <NuxtLink :to="tipCard.linked_page"
+                v-html="tipCard.button_text"
+                class="py-3 w-full"
+      >
 
+      </NuxtLink>
     </div>
+
   </div>
 </template>
 
