2effad804d1b31d1f102f57860735e99aef1c521
Ashish Aryal <ashish@kortaben.se>
2021-10-14 10:15:45 +0200

    bugs fixes

--- components/landmarks/LandmarkRouteDetail.vue
+++ components/landmarks/LandmarkRouteDetail.vue
@@ -2,8 +2,8 @@
 <div>
 <!--  <span class="w-4 h-4 mr-4  rounded-full inline-block" :class="routeDetails.colour.value"> </span>-->
 <!--  <span class="text-stBrown text-xl font-medium leading-6">{{ routeDetails.colour.label }}</span>-->
-  <ul>
-    <li v-for="trail in routeDetails">
+  <ul class="flex flex-wrap">
+    <li v-for="trail in routeDetails" class="w-1/2">
       <span class="w-4 h-4 mr-4  rounded-full inline-block" :class="trail.acf.colour.value"> </span>
       <span class="text-stBrown text-xl font-medium leading-6">{{ trail.acf.colour.label }}</span>
     </li>
--- components/landmarks/TheLandmarkRouteInfo.vue
+++ components/landmarks/TheLandmarkRouteInfo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="border-b lg:border-0 border-stGreen pt-3">
-    <ul>
-      <li v-for="trail in vandringslederna">
+    <ul class="flex flex-wrap">
+      <li v-for="trail in vandringslederna" class="w-1/2 ">
         <span class="w-4 h-4 mr-4  rounded-full inline-block" :class="trail.acf.colour.value"> </span>
         <span class="text-stBrown text-xl font-medium leading-6">{{ trail.acf.colour.label }}</span>
       </li>
