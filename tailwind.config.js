d852be9c86e143de3116eaa2e03175898df172dc
Ashish Aryal <ashish@kortaben.se>
2021-10-18 11:40:55 +0200

    added purge locations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  mode: 'jit',
-  purge: {},
+  purge: {
+	   content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js'
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors:{
